696be711a40efa0959f8df98f995cb340f4ff517
yoshi202o <90802548+yoshi202o@users.noreply.github.com>
2022-01-18 19:44:31 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "BANDANA WADDLE DEE";
+const accessCode2 = "RAREZA";
+const accessCode3 = "KIRBY";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text(" A N N B A D A  E D D L W A  D E E", 100,50)
+    fill("lightblue")
+    text("tesoropista:cual personaje tiene una lanza en los juegos de kirby !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A E Z R A R", 700,150)
+    fill("lightblue")
+    text("tesoropista:cual es el cuarto fragmento del juego kirby clash !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("Y R B K I", 100,250)
+    fill("lightblue")
+    text("tesoropista: de que juego son las gemas manzana !!", 100,270)
+
+}
